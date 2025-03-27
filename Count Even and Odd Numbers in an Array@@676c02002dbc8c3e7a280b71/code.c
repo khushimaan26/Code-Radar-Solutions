767359597158756542f5a26af4c3c4c60d4ca2d9
khushimaan26 <khushi1355.becse24@chitkara.edu.in>
2025-03-27 20:01:59 +0530

    Solution to Count Even and Odd Numbers in an Array

--- Count Even and Odd Numbers in an Array@@676c02002dbc8c3e7a280b71/code.c
+++ Count Even and Odd Numbers in an Array@@676c02002dbc8c3e7a280b71/code.c
@@ -1,10 +1,10 @@
 
 N = int(input())  # Read the number of elements
-arr = list(map(int, input().split()))  # Read the array elements
+arr = list(map(int, input().split()))  
 
 
 even_count = sum(1 for x in arr if x % 2 == 0)
-odd_count = N - even_count  # Remaining are odd
+odd_count = N - even_count  
 
 
 print(even_count, odd_count)
